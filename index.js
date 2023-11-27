864f4a4731850e744ae2c3fdaa6be4a7f2e9a01f
Muslehud77 <fardinmohit@gmail.com>
2023-11-28 03:43:11 +0800

    get method for allContest and popular contest

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 5000
 
 
@@ -12,6 +13,89 @@ app.use(express.json())
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+const uri = process.env.DB_URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const talentSphere = client.db("Talent-Sphere")
+    const contestCollection = talentSphere.collection("contest")
+
+    //*contest related api calls
+
+    app.get('/contest',async(req,res)=>{
+        const result = await contestCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.get('/contest/:id',async(req,res)=>{
+        const id = {_id: new ObjectId(req.params.id)}
+        const result = await contestCollection.findOne(id)
+        res.send(result)
+    })
+
+    app.get('/popular',async(req,res)=>{
+        const result = await contestCollection
+          .find(
+            {},
+            {
+              projection: {
+                _id: 1,
+                contestName: 1,
+                contestImg: 1,
+                attempt: 1,
+                tags: 1,
+                shortDescription:1,
+              },
+            }
+          )
+          .sort({ attempt: -1 })
+          .limit(6)
+          .toArray();
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
